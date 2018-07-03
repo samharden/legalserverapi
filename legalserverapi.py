144c8f4b1b7d27e147610a6d108e1b5b1384ed24
samharden <samharden@gmail.com>
2018-07-03 13:49:29 -0400

    Now with subclass

--- legalserverapi.py
+++ legalserverapi.py
@@ -1,13 +1,7 @@
-from bs4 import BeautifulSoup as bsoup
 import requests
-import os
-import selenium
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
 class LegServ:
-    ## Example:
-    ## query = LegServ('username', 'password', 'domain')
+
     ## Note: make sure your username and password don't contain special characters
 
     def __init__(self, username, password, domain):
@@ -16,9 +10,24 @@ class LegServ:
         self.dm = domain
         self.initial_domain = "https://"+username+":"+password+"@"+domain
 
-    def check_for_cases(self, name_to_query):
+    def __str__(self):
+        return self.un, self.pw, self.initial_domain
 
-        name_to_query = name_to_query.replace(" ", "+")
+
+class Person(LegServ):
+
+    ## Example: new_client_1 = Person('username', 'password', 'domain.legalserver.org', 'John', 'Doe')
+
+    def __init__(self, username, password, domain, firstname, lastname):
+        LegServ.__init__(self, username, password, domain)
+        self.firstname = firstname
+        self.lastname = lastname
+
+    def check_for_cases(self):
+
+        ## new_client_1.check_for_cases()
+
+        name_to_query = self.firstname+"+"+self.lastname
         r = requests.get(self.initial_domain+"/matter/api/matter_search/?token="+name_to_query)
         r_json = r.json()
         results_list = r_json['results']
@@ -26,18 +35,14 @@ class LegServ:
         if len(results_list) > 0:
             for result in results_list:
                 final_list.append(r_json['results'][0])
-
         return final_list
 
 
-    def get_basic_case_info_by_name(self, name_to_query, return_value):
-
-        ## Usage:
-        ## query.get_basic_case_info_by_name('John Doe', 'id')
-
+    def get_basic_case_info_by_name(self, return_value):
+        ## new_client_1.get_basic_case_info_by_name('id')
         ## returns the requested value(s) in a list
 
-        name_to_query = name_to_query.replace(" ", "+")
+        name_to_query = self.firstname+"+"+self.lastname
         r = requests.get(self.initial_domain+"/matter/api/basic_case_info/?client_name="+name_to_query)
         r_json = r.json()
         results_list = r_json['results']
@@ -51,10 +56,29 @@ class LegServ:
 
         return final_list
 
-    def create_case_note(self, case_number, note):
-        ## creates a case note in the case with the given case_number
+    def create_case_note(self, note):
+        ## creates a case note
+        ## new_client_1.create_case_note('Hi this is a note')
+
+        case_number = Person.get_basic_case_info_by_name(self, 'identification_number')[0]
         try:
             r = requests.post(self.initial_domain+"/matter/api/create_case_note/?case_number="+case_number+"&note="+note)
+        except Exception as e:
+            print(str(e))
+
+    def set_probono_opportunity(self, summary, question):
+        case_number = Person.get_basic_case_info_by_name(self, 'id')[0]
+        try:
+            r = requests.post(self.initial_domain+"/matter/api/set_opportunity/?caseid="+case_number+"&summary="+summary+"&question="+question)
+            print(r.json())
+        except Exception as e:
+            print(str(e))
+
+    def basic_admin_info(self):
+        case_number = Person.get_basic_case_info_by_name(self, 'id')[0]
+        try:
+            r = requests.get(self.initial_domain+"/matter/api/basic_admin_info/?caseid="+case_number)
+            return r.json()['results']
         except Exception as e:
             print(str(e))
 
@@ -65,8 +89,6 @@ class LegServ:
             print(r.json())
         except Exception as e:
             print(str(e))
-
-
 
 
 
